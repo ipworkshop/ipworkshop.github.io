242514ab550b2607a7e2c48b4a039f1876517adf
Alexandru Radovici <alex@ipworkshop.ro>
2025-07-18 16:37:15 +0300

    fix domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://dev.ipworkshop.ro',
+  url: 'https://developer.ipworkshop.ro',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
